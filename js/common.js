aec04a30cc18152f1cccf6ac7d7ee0344f6f230b
Grom <gromimol@ukr.net>
2021-04-01 17:03:33 +0300

    Каталог

--- js/common.js
+++ js/common.js
@@ -86,4 +86,19 @@ $(document).ready(function() {
         $(this).toggleClass('active');
         $(this).parent().find('.footer__item__line').toggleClass('active');
     })
+
+    // fake select
+    $('.select').on('click','.placeholder',function(){
+        var parent = $(this).closest('.select');
+        if ( ! parent.hasClass('is-open')){
+          parent.addClass('is-open');
+          $('.select.is-open').not(parent).removeClass('is-open');
+        }else{
+          parent.removeClass('is-open');
+        }
+      }).on('click','ul>li',function(){
+        var parent = $(this).closest('.select');
+        parent.removeClass('is-open').find('.placeholder').text( $(this).text() );
+        parent.find('input[type=hidden]').attr('value', $(this).attr('data-value') );
+      });
 })
